ff35fcfa04c6ec02559af2681c2e6e831c2e737c
jvprimakipr <j.joaoviniciuspr@hotmail.com>
2020-09-22 17:27:37 +0000

    states and countries

---
+++ scraping/.ipynb_checkpoints/scrape-checkpoint.py
@@ -0,0 +1,142 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from time import time
+
+options = Options()
+options.headless = True
+options.add_argument("--window-size=1920,1200")
+driver = webdriver.Chrome(options=options, executable_path='//bin/chromedriver')
+
+tailnumbers = [line.rstrip() for line in open('tail_numbers.txt')]
+#tailnumbers = ['N29975', 'N12142', 'N11106','N223UA','N7705A']
+
+f=open('failure.txt','w')
+
+data = {
+    'companies' : set(), # Set of tuples like (name,city,state,country)
+    'engine_types' : set(), # Set of engine types
+    'aircraft_types' : set(), # Set of aircraft types
+    'aircraft_models' : set(), # Set of tuples like (Manufacturer,Aircraft_model)
+    'manufacturers' : set(), # Set of Manufacturers
+    'aircrafts' : set(), # Set of tuples like (N-Number,Company_name,Airplane_Model,Engine_Type,Aircraft_Type,Year)
+}
+
+qtd=len(tailnumbers)
+t0=time()
+for aircraft in range(qtd):
+    try:
+        driver.get("https://registry.faa.gov/aircraftinquiry/NNum_Results.aspx?NNumbertxt="+tailnumbers[aircraft])
+        data['companies'].add((
+            driver.find_element_by_id('ctl00_content_lbOwnerName').text,
+            driver.find_element_by_id('ctl00_content_lbOwnerCity').text,
+            driver.find_element_by_id('ctl00_content_lbOwnerState').text,
+            driver.find_element_by_id('ctl00_content_lbOwnerCountry').text
+        ))
+
+        data['engine_types'].add(
+            driver.find_element_by_id('ctl00_content_lbTypeEng').text
+        )
+
+        data['aircraft_types'].add(
+            driver.find_element_by_id('ctl00_content_Label11').text
+        )
+
+        data['aircraft_models'].add((
+            driver.find_element_by_id('ctl00_content_lbMfrName').text,
+            driver.find_element_by_id('ctl00_content_Label7').text
+        ))
+
+        data['manufacturers'].add(
+            driver.find_element_by_id('ctl00_content_lbMfrName').text
+        )
+
+        data['aircrafts'].add((
+            tailnumbers[aircraft],
+            driver.find_element_by_id('ctl00_content_lbOwnerName').text,
+            driver.find_element_by_id('ctl00_content_Label7').text,
+            driver.find_element_by_id('ctl00_content_lbTypeEng').text,
+            driver.find_element_by_id('ctl00_content_Label11').text,
+            driver.find_element_by_id('ctl00_content_Label17').text
+        ))
+
+        print('Processed: '+tailnumbers[aircraft]+"     OK     "+str(aircraft+1)+' de '+str(qtd)+' - '+'{:.2f} %'.format((aircraft+1)*100/qtd)+'     {:.2f} min'.format((time()-t0)/60))
+    
+    except:
+        try:
+            driver.get("https://registry.faa.gov/aircraftinquiry/NNum_Results.aspx?NNumbertxt="+tailnumbers[aircraft])
+            data['companies'].add((
+                driver.find_element_by_id('ctl00_content_drptrDeRegAircraft_ctl01_lbDROwnerName').text,
+                driver.find_element_by_id('ctl00_content_drptrDeRegAircraft_ctl01_lbDROwnerCity').text,
+                driver.find_element_by_id('ctl00_content_drptrDeRegAircraft_ctl01_lbDROwnerState').text,
+                driver.find_element_by_id('ctl00_content_drptrDeRegAircraft_ctl01_lbDROwnerCountry').text
+            ))
+
+            data['aircraft_models'].add((
+                driver.find_element_by_id('ctl00_content_drptrDeRegAircraft_ctl01_lbDeRegMfrName').text,
+                driver.find_element_by_id('ctl00_content_drptrDeRegAircraft_ctl01_lbDeRegModel').text
+            ))
+
+            data['manufacturers'].add(
+                driver.find_element_by_id('ctl00_content_drptrDeRegAircraft_ctl01_lbDeRegMfrName').text
+            )
+
+            data['aircrafts'].add((
+                tailnumbers[aircraft],
+                driver.find_element_by_id('ctl00_content_drptrDeRegAircraft_ctl01_lbDROwnerName').text,
+                driver.find_element_by_id('ctl00_content_drptrDeRegAircraft_ctl01_lbDeRegModel').text,
+                'NULL',
+                'NULL',
+                driver.find_element_by_id('ctl00_content_drptrDeRegAircraft_ctl01_lbDeRegYearMfr').text
+            ))
+
+            print('Processed: '+tailnumbers[aircraft]+"     OK     "+str(aircraft+1)+' de '+str(qtd)+' - '+'{:.2f} %'.format((aircraft+1)*100/qtd)+'     {:.2f} min'.format((time()-t0)/60))
+
+        except:
+            f.write(tailnumbers[aircraft]+'\n')
+            print('Processed: '+tailnumbers[aircraft]+"    ERROR   "+str(aircraft+1)+' de '+str(qtd)+' - '+'{:.2f} %'.format((aircraft+1)*100/qtd)+'     {:.2f} s'.format((time()-t0)/60))
+
+f.close()
+
+companies = open('companies.csv','w')
+companies.write("Name,City,State,Country\n")
+for tuples in data['companies']:
+    companies.write(tuples[0]+','+tuples[1]+','+tuples[2]+','+tuples[3]+'\n')
+companies.close()
+
+
+engine_types = open('engine_types.csv','w')
+engine_types.write("Engine_Types\n")
+for types in data['engine_types']:
+    engine_types.write(types+'\n')
+engine_types.close()
+
+
+aircraft_types = open('aircraft_types.csv','w')
+aircraft_types.write("Aircraft_Type\n")
+for types in data['aircraft_types']:
+    aircraft_types.write(types+'\n')
+aircraft_types.close()
+
+
+aircraft_models = open('aircraft_models.csv','w')
+aircraft_models.write("Manufacturer,Aircraft_Model\n")
+for tuples in data['aircraft_models']:
+    aircraft_models.write(tuples[0]+','+tuples[1]+'\n')
+aircraft_models.close()
+
+
+manufacturers = open('manufacturers.csv','w')
+manufacturers.write("Manufacturer\n")
+for name in data['manufacturers']:
+    manufacturers.write(name+'\n')
+manufacturers.close()
+
+
+aircrafts = open('aircrafts.csv','w')
+aircrafts.write("N-Number,Company_name,Airplane_Model,Engine_Type,Aircraft_Type,Year\n")
+for tuples in data['aircrafts']:
+    aircrafts.write(tuples[0]+','+tuples[1]+','+tuples[2]+','+tuples[3]+','+tuples[4]+','+tuples[5]+'\n')
+aircrafts.close()
+
+
+driver.quit()
